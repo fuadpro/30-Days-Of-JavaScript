fdd67159cde2725a8b3e1dc1003791721bce2f92
Fuad Memmedov <m.fuad@code.edu.az>
2023-02-19 13:33:19 +0400

    Create exercises.js

---
+++ 03_Day_Booleans_operators_date/03_day_starter/exercises.js
@@ -0,0 +1,15 @@
+Exercises: Level 1
+
+
+
+
+
+
+// Exercises: Level 2 
+// 1. 
+//     let base = prompt ("Base daxil et : ", "0");
+//     let hund = prompt ("H daxil et : ", "0");
+
+//     let area = 0.5 * base * hund;
+//     console.log(area);
+// 2.
