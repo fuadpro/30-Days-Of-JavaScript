242bafb15168aee915f28f12dcac58e5f604e6d0
Fuad Memmedov <m.fuad@code.edu.az>
2023-02-19 17:24:44 +0400

    Update exercises.js

--- 04_Day_Conditionals/exercises.js
+++ 04_Day_Conditionals/exercises.js
@@ -1,26 +1,57 @@
 //                    Exercises: Level 1
 // 1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
-
-// Enter your age: 30
-// You are old enough to drive.
-
-// Enter your age:15
-// You are left with 3 years to drive.
-
+// Enter your age: 30 // You are old enough to drive.
+// Enter your age:15 // You are left with 3 years to drive.
+//               let age = prompt ('Enter your age');
+//               let ageMin= 18 - age;
+//               if (age >= 18){
+//                   console.log('You are old enough to drive.')
+//               }else {
+//                   console.log(`You are left with ${ageMin} years to drive.`)
+//               }
 
 // 2. Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
-
 // Enter your age: 30
 // You are 5 years older than me.
+//               let age = prompt ('Enter your age');
+//               let myAge = 25 ;
+//               console.log(`Enter your age: ${age}`);
+//               if (age > myAge){
+//               console.log(`You are ${age - myAge} years older than me.`)
+//               }else if (age == myAge){
+//                   console.log(`You are equal than me.`)
+//               }else{
+//                   console.log(`You are ${myAge - age} years little than me.`)
+//               }
 
 
-// 3. If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+// 3. If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways 
+//                using if else
 
-// using if else
-// ternary operator.
-//   let a = 4
-//   let b = 3
-//   4 is greater than 3
+//               let a = 4;
+//               let b = 3;     //  let b = 5;
+//               if(a > b){
+//                   console.log(`${a} is greater than ${b}`)
+//               }else if(a < b){
+//                   console.log(`${a} is less than ${b}`)
+//               }else{
+//                   console.log(`${a} is equal to ${b}`)
+//               }
+//           ternary operator
+
+//                 let a = 4;
+//                 let b = 3;
+//                 let result = a > b ? `${a} is greater than ${b}` : `${a} is less than ${b}`;
+
+//          nested ternary operators
+
+//                 let a = 4;
+//                 let b = 3;
+//                 let result = a > b ? `${a} is greater than ${b}` : 
+//                              a < b ?  `${a} is less than ${b}` : 
+//                              `${a} is equal to ${b}`;
+
+
 
 
 // 4. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
