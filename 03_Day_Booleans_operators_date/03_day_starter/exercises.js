003b5a8c11da41c990374924880ad3dcff552af0
Fuad Memmedov <m.fuad@code.edu.az>
2023-02-19 14:59:48 +0400

    Update exercises.js

--- 03_Day_Booleans_operators_date/03_day_starter/exercises.js
+++ 03_Day_Booleans_operators_date/03_day_starter/exercises.js
@@ -195,19 +195,19 @@
 // DD/MM/YYYY HH:mm
 
 
-//                                          Exercises: Level 3
+//                                           Exercises: Level 3
 
 // 1. Create a human readable time format using the Date time object. The hour and the minute should be all the time two 
 // digits(7 hours should be 07 and 5 minutes should be 05 )
 
-const now = new Date();
-const year = now.getFullYear();
-const month = (now.getMonth() + 1).toString().padStart(2, '0');
-const day = now.getDate().toString().padStart(2, '0');
-const hours = now.getHours().toString().padStart(2, '0');
-const minutes = now.getMinutes().toString().padStart(2, '0');
-const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes} ${hours >= 12 ? 'PM' : 'AM'}`;
+// const now = new Date();
+// const year = now.getFullYear();
+// const month = (now.getMonth() + 1).toString().padStart(2, '0');
+// const day = now.getDate().toString().padStart(2, '0');
+// const hours = now.getHours().toString().padStart(2, '0');
+// const minutes = now.getMinutes().toString().padStart(2, '0');
+// const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes} ${hours >= 12 ? 'PM' : 'AM'}`;
 
-console.log(formattedDateTime);
+// console.log(formattedDateTime);
 
 // YYY-MM-DD HH:mm eg. 20120-01-02 07:05
