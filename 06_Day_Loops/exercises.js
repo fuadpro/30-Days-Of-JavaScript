502b17fae307f442407e0eea851886d792ad3e16
Fuad Memmedov <m.fuad@code.edu.az>
2023-02-21 22:34:21 +0400

    Update exercises.js

--- 06_Day_Loops/exercises.js
+++ 06_Day_Loops/exercises.js
@@ -63,10 +63,18 @@
 //           }
 
 // 7. Use for loop to iterate from 0 to 100 and print only even numbers
-
+            // for (let i = 0; i <= 100; i++) {
+            //         if (i % 2 === 0) {
+            //             console.log(i);
+            //         }
+            //     }
 
 // 8. Use for loop to iterate from 0 to 100 and print only odd numbers
-
+            // for (let i = 0; i <= 100; i++) {
+            //     if (i % 2 !== 0) {
+            //     console.log(i);
+            //     }
+            // }
 
 // 9. Use for loop to iterate from 0 to 100 and print only prime numbers
 
